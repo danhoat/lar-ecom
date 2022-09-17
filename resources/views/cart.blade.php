252c62fdc0b4c3467db048d86b3214d268848811
danhoat <danhoat@gmail.com>
2022-09-17 19:12:20 +0700

    view cart

---
+++ resources/views/cart.blade.php
@@ -0,0 +1,34 @@
+@extends('master')
+@section('content')
+<?php $total = 0;?>
+
+<div class="container main cart">
+    @if($products)
+       <table class="table table-striped_">
+            <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Gallery</th>
+                  <th scope="col">Price</th>
+                </tr>
+              </thead>
+
+            @foreach($products as $key=>$item)
+                <tr> <?php $total = $total + $item->price;?>
+                 <th scope="row">{{++$key}}</th>
+
+                <td>{{$item->name}}</td>
+                <td><img src="{{$item->gallery}}" /></td>
+                <td>{{$item->price}} </td>
+                </tr>
+            @endforeach
+            <tr> <td colspan="3">Total:</td>
+                <td><?php echo $total;?></td>
+            </tr>
+         </table>
+    @endif
+</div>
+
+
+@endsection
