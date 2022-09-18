53141ae874e329bcfc20119cfbdd21188b099464
danhoat <danhoat@gmail.com>
2022-09-18 09:30:04 +0700

    complete ajax login

--- resources/views/ajaxform.blade.php
+++ resources/views/ajaxform.blade.php
@@ -5,16 +5,16 @@
     <div id="login-column" class="col-md-6">
         <h3 class="text-center text-info">Login</h3>
         <div id="login-box" class="login-box">
-            <form id="loginAjax" class="form">
+            <form id="loginAjax" class="form needs-validation">
               @csrf
 
                 <div class="form-group">
                     <label for="email" class="text-info">Email or Username</label><br>
-                    <input type="text"  name="email" id="email" class="form-control">
+                    <input type="text"  name="email" id="email" class="form-control required required" required>
                 </div>
                 <div class="form-group">
                     <label for="password" class="text-info">Password:</label><br>
-                    <input type="text" name="password" id="password" class="form-control">
+                    <input type="text" name="password" id="password" class="form-control" required>
                 </div>
                  <div class="form-group alert alert-success" style="display:none;"></div>
                 <div class="form-group text-right">
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -33,6 +33,18 @@ if( $user ){
       <li class="nav-item">
         <a class="nav-link" href="/ajaxform">Ajax Form</a>
       </li>
+
+       <li class="nav-item dropdown">
+
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          User
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+
+          <a class="dropdown-item" href="/logout">Thêm User</a>
+        </div>
+      </li>
+
       <li class="searchForm">
         <form class="form-inline my-2 my-lg-0" action="/search" method="GET">
         <input class="form-control mr-sm-2" name="keyword" type="search" placeholder="Search" aria-label="Search">
